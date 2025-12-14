3e0ae58c09e6ea781b84eef62284568dee352a19
LeopoldPillet <35914645+LeopoldPillet@users.noreply.github.com>
2025-12-14 18:49:30 +0100

    Bash file

--- install_package.sh
+++ install_package.sh
@@ -1,1 +1,13 @@
-%%bash# Verifying package installationinstall_package() {  for i in "$@"; do    if ! pip show "$i" > /dev/null 2>> errors.log; then      pip install "$i" > /dev/null 2>> errors.log      echo "$i installed"    else      echo "$i already installed"    fi  done}#install_package ffmpeg opencv-python#output:#ffmpeg already installed#opencv-python already installed
+#!/usr/bin/env bash
+
+# Verifying package installation
+install_package() {
+  for i in "$@"; do
+    if ! python -m pip show "$i" > /dev/null 2>> errors.log; then
+      python -m pip install "$i" > /dev/null 2>> errors.log
+      echo "$i installed"
+    else
+      echo "$i already installed"
+    fi
+  done
+}
