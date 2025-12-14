9041bdb5b5f54f6dad53ce17fb28039b61d078d5
LeopoldPillet <35914645+LeopoldPillet@users.noreply.github.com>
2025-12-14 18:38:27 +0100

    Made this file unix readable

--- install_package.sh
+++ install_package.sh
@@ -1,17 +1,1 @@
-%%bash
-# Verifying package installation
-install_package() {
-  for i in "$@"; do
-    if ! pip show "$i" > /dev/null 2>> errors.log; then
-      pip install "$i" > /dev/null 2>> errors.log
-      echo "$i installed"
-    else
-      echo "$i already installed"
-    fi
-  done
-}
-
-#install_package ffmpeg opencv-python
-#output:
-#ffmpeg already installed
-#opencv-python already installed
+%%bash# Verifying package installationinstall_package() {  for i in "$@"; do    if ! pip show "$i" > /dev/null 2>> errors.log; then      pip install "$i" > /dev/null 2>> errors.log      echo "$i installed"    else      echo "$i already installed"    fi  done}#install_package ffmpeg opencv-python#output:#ffmpeg already installed#opencv-python already installed
