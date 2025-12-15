e6d3cf7f00c1bc0b4c2284e5765be42cc81ae4d9
LeopoldPillet <35914645+LeopoldPillet@users.noreply.github.com>
2025-12-15 15:42:15 +0100

    Package not found managed

--- install_package.sh
+++ install_package.sh
@@ -4,8 +4,11 @@
 install_package() {
   for i in "$@"; do
     if ! python -m pip show "$i" > /dev/null 2>> errors.log; then
-      python -m pip install "$i" > /dev/null 2>> errors.log
-      echo "$i installed"
+      if ! python -m pip install "$i" > /dev/null 2>>errors.log; then
+        echo "$i not found"
+      else
+        echo "$i installed"
+      fi
     else
       echo "$i already installed"
     fi
